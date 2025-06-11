d8b99573607016e4fb1e062636c6b5c696afc828
sung0916 <qjeltk52@naver.com>
2025-06-12 01:53:56 +0900

    깃페이지 파일생성

---
+++ backend/src/main/java/io/github/sung0916/Sung0916Application.java
@@ -0,0 +1,13 @@
+package io.github.sung0916;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sung0916Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sung0916Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/sung0916/Sung0916ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sung0916;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sung0916ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
